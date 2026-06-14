e00319e966c60079bb2e997e7f471fc70fd3d876
mariusbudiun-droid <mariusbudiun@gmail.com>
2026-06-14 12:08:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // MAGS — Service Worker
 // Cache-bump: incrementa CACHE_VERSION a ogni release.
 // ============================================================
-const CACHE_VERSION = 'mags-v0.9.36';
+const CACHE_VERSION = 'mags-v0.9.37';
 const ASSETS = [
   './',
   './index.html',
